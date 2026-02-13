fb5e9b325d8359f093a154a5980495347d2346a0
xigma0512 <xig1517@gmail.com>
2026-02-14 02:55:59 +0800

    修正 JwtStrategy 的 validate 方法，將返回的 userId 改為 id

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -23,6 +23,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: JwtPayload) {
-    return { userId: payload.sub, email: payload.email, role: payload.role };
+    return { id: payload.sub, email: payload.email, role: payload.role };
   }
 }
